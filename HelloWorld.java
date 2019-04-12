3f3e4ea779788f8c86f4f14770a7097a245a81d7
Maxwell Todd <mtmaxtodd@gmail.com>
2019-04-11 19:37:58 -0500

    Added an array and printed a line with the first entry in the array.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        //array containing names of technologies I am experienced with.
+        String[] technologies = {"React", "JavaScript", "Node.JS",
+                                 "MySQL", "PotgreSQL", "Express",
+                                 "Git", "npm"};
+        
+        System.out.println(technologies[0]);
+
     }
 
 }
