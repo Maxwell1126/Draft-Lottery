59a28f316ee5c9ac893fa5fa26fb33b9c38757c4
Maxwell Todd <mtmaxtodd@gmail.com>
2019-04-12 16:27:51 -0500

    Added a conditional

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,14 +3,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-        //array containing names of technologies I am experienced with.
-        String[] technologies = {"React", "JavaScript", "Node.JS",
-                                 "MySQL", "PotgreSQL", "Express",
-                                 "Git", "npm"};
-        for (int i=0; i<technologies.length; i++){
-            System.out.println(technologies[i]);
-        } 
-
+        // array containing names of technologies I am experienced with.
+        String[] technologies = { "React", "JavaScript", "Node.JS", "MySQL", "PotgreSQL", "Express", "Git", "npm" };
+        boolean shouldPrint = true;
+        if (shouldPrint) {
+            for (int i = 0; i < technologies.length; i++) {
+                System.out.println(technologies[i]);
+            }
+        }
     }
 
 }
