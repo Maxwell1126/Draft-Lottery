d09b50b24335c18b605edd919c2c6397e5e78d76
Maxwell Todd <mtmaxtodd@gmail.com>
2019-04-13 16:49:03 -0500

    Added math.random and made a conditional based one what number is produced.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.function.ToIntFunction;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
@@ -11,6 +13,19 @@ public class HelloWorld {
                 System.out.println(technologies[i]);
             }
         }
+
+        double x = (int) (Math.random() * ((12 - 1) + 1)) + 1;
+        System.out.println(x);
+        if (x == (1) | x == (2) | x == (3)){
+            System.out.println("Team Blue wins.");
+        } else if (x == (4) | x == (5) | x == (6)){
+            System.out.println("Team Red wins.");
+        } else if (x == (7) | x == (8) | x == (9)){
+            System.out.println("Team Turquoise wins.");
+        } else{
+            System.out.println("Team Tan wins.");
+        }
+        
     }
 
 }
