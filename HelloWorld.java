91266980dd3ab8dba0190ebcc4c8de9c5e075738
Maxwell Todd <mtmaxtodd@gmail.com>
2019-04-13 17:15:49 -0500

    Added another random number and an AND conditional. I also added more teams to be drawn.

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,17 +15,27 @@ public class HelloWorld {
         }
 
         double x = (int) (Math.random() * ((12 - 1) + 1)) + 1;
-        System.out.println(x);
-        if (x == (1) | x == (2) | x == (3)){
+        double y = (int) (Math.random() * ((4 - 1) + 1)) + 1;
+        System.out.println("number x "+x);
+        System.out.println("number y "+y);
+        if ((x == (1) | x == (2) | x == (3)) && (y == 1) | (y == 2)) {
             System.out.println("Team Blue wins.");
-        } else if (x == (4) | x == (5) | x == (6)){
+        } else if ((x == (1) | x == (2) | x == (3)) && (y == 3) | (y == 4)) {
+            System.out.println("Team Gray wins.");
+        } else if (x == (4) | x == (5) | x == (6) && (y==1) | (y==2)) {
             System.out.println("Team Red wins.");
-        } else if (x == (7) | x == (8) | x == (9)){
+        }else if(x == (4) | x == (5) | x == (6) && (y == 3) | (y == 4)){
+            System.out.println("Team Purple wins.");
+        } else if (x == (7) | x == (8) | x == (9) && (y==1) | (y==2)) {
             System.out.println("Team Turquoise wins.");
-        } else{
+        }else if(x == (7) | x == (8) | x == (9) && (y==3) | (y==4)){
+            System.out.println("Team Green wins");
+        } else if (x == (10) | x == (11) | x == (12) && (y == 1) | (y == 2)){
             System.out.println("Team Tan wins.");
+        } else{
+            System.out.println("Team Orange wins.");
         }
-        
+
     }
 
 }
