ee4b0140b9b46947a646a6ee82cd0eff2708a8e6
Maxwell Todd <mtmaxtodd@gmail.com>
2019-04-15 16:32:42 -0500

    Deleted test code. renamed the java file and compiled it. Deleted the old compiled file.

--- DraftLottery.java
+++ DraftLottery.java
@@ -1,18 +1,8 @@
 import java.util.function.ToIntFunction;
 
-public class HelloWorld {
+public class DraftLottery {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-        // array containing names of technologies I am experienced with.
-        String[] technologies = { "React", "JavaScript", "Node.JS", "MySQL", "PotgreSQL", "Express", "Git", "npm" };
-        boolean shouldPrint = true;
-        if (shouldPrint) {
-            for (int i = 0; i < technologies.length; i++) {
-                System.out.println(technologies[i]);
-            }
-        }
 
         String firstPlace = "";
         String secondPlace = "";
